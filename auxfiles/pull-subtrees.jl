0c9e1b36f3f5abe58f71a07f7715264ffedab827
Tillmann Mühlpfordt <tillmann.muehlpfordt@kit.edu>
2020-04-23 07:11:38 +0200

    move subtree information to JSON file

--- auxfiles/pull-subtrees.jl
+++ auxfiles/pull-subtrees.jl
@@ -1,13 +1,17 @@
-timueh_seir = Dict("git-subtree-dir"=> "julia/SEIR-with-uncertainties", "git-subtree-split"=> "8a5cdeaa616f675514ae2394544de0f25bbd0d83", "url"=> "https://github.com/timueh/PandemicModeling")
+# timueh_seir = Dict("git-subtree-dir"=> "julia/SEIR-with-uncertainties", "git-subtree-split"=> "8a5cdeaa616f675514ae2394544de0f25bbd0d83", "url"=> "https://github.com/timueh/PandemicModeling")
 
-qed_hamburg = Dict("git-subtree-dir"=> "matlab/QEDhamburg", "git-subtree-split"=> "f539f28df31dfa2f810de7a50a331b38b60d0ebf", "url"=> "https://github.com/QEDHamburg/covid19")
+# qed_hamburg = Dict("git-subtree-dir"=> "matlab/QEDhamburg", "git-subtree-split"=> "f539f28df31dfa2f810de7a50a331b38b60d0ebf", "url"=> "https://github.com/QEDHamburg/covid19")
 
-mitepid_sim = Dict("git-subtree-dir"=> "python/MiTepid_sim", "git-subtree-split"=> "c5e90bb33b0d90f1ad1ef358fec19147bee05b7a", "url"=> "https://github.com/vahid-sb/MiTepid_sim.git")
+# mitepid_sim = Dict("git-subtree-dir"=> "python/MiTepid_sim", "git-subtree-split"=> "c5e90bb33b0d90f1ad1ef358fec19147bee05b7a", "url"=> "https://github.com/vahid-sb/MiTepid_sim.git")
 
-repos = [timueh_seir, qed_hamburg, mitepid_sim]
+# repos = [timueh_seir, qed_hamburg, mitepid_sim]
 
-for repo in repos
-    dir, url = repo["git-subtree-dir"], repo["url"]
+using JSON
+repos = JSON.parsefile("auxfiles/subtree-packages.json")
+
+for (name, data) in repos
+    dir, url = data["language"] * "/" * name, data["url"]
+    display((dir, url))
     cmd = `git subtree pull --prefix=$dir $url master --squash`
     printstyled("Pulling $dir\n", color=:green)
     result = run(cmd)
